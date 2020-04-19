8f59e459e130ebb6ff8138114ebee85755ce03eb
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-04-19 19:44:55 +0200

    activation response only @ResponseStatus(HttpStatus.OK)

--- src/main/java/pl/termosteam/kinex/controller/administration/accounts/register/ActivateController.java
+++ src/main/java/pl/termosteam/kinex/controller/administration/accounts/register/ActivateController.java
@@ -2,11 +2,8 @@ package pl.termosteam.kinex.controller.administration.accounts.register;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import pl.termosteam.kinex.dto.JwtActivationRequestDto;
 import pl.termosteam.kinex.service.UserService;
 
@@ -21,11 +18,11 @@ public class ActivateController {
     private final UserService userService;
 
     @PostMapping(value = "activate")
-    public ResponseEntity<?> activateUserAccount(
+    @ResponseStatus(HttpStatus.OK)
+    public void activateUserAccount(
             @RequestBody @Valid JwtActivationRequestDto authenticationRequest) {
         userService.activateByToken(authenticationRequest.getUsername(),
                 authenticationRequest.getActivation_token());
         log.info("ActivateController->activateUserAccount: account activated for the username: " + authenticationRequest.getUsername());
-        return ResponseEntity.ok("Account Activated");
     }
 }
