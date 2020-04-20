1c14745ca02109eb5c34e9a4950be43c89890568
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-04-20 20:16:22 +0200

    removed @DurationUnit(ChronoUnit.MINUTES)

--- src/main/java/pl/termosteam/kinex/configuration/properties/JwtConfiguration.java
+++ src/main/java/pl/termosteam/kinex/configuration/properties/JwtConfiguration.java
@@ -2,22 +2,18 @@ package pl.termosteam.kinex.configuration.properties;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.convert.DurationUnit;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 @Configuration
 @Getter
 public class JwtConfiguration {
 
     @Value("${jwt.token.validity.time.minutes:PT300M}")
-    @DurationUnit(ChronoUnit.MINUTES)
     private Duration JWT_TOKEN_VALIDITY;
 
     @Value("${jwt.token.validity.activation.time.minutes:PT25M}")
-    @DurationUnit(ChronoUnit.MINUTES)
     private Duration JWT_TOKEN_VALIDITY_ACTIVATION_TIME;
 
     @Value("${jwt.secret}")
